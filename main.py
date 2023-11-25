260dedab83463d4bcbd8818db0eb36c0f7f41670
snikersovich <146052110+snikersovich@users.noreply.github.com>
2023-11-25 21:59:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 #Линейный поиск
-def linear_Search(list1, n, key):
-    for i in range(0, n):
-        if (list1[i] == key):
+def LinearSearch(lys, element):
+    for i in range (len(lys)):
+        if lys[i] == element:
             return i
     return -1
-list1 = [1, 3, 5, 4, 7, 9]
-key = 7
-n = len(list1)
-res = linear_Search(list1, n, key)
-if (res == -1):
-    print("Element not found")
-else:
-    print("Element found at index: ", res)
-  
+    print(LinearSearch([1,2,3,4,5,2,1]
+
+#Бинарный поиск
+def BinarySearch(lys, val):
+    first = 0
+    last = len(lys)-1
+    index = -1
+    while (first <= last) and (index == -1):
+        mid = (first+last)//2
+        if lys[mid] == val:
+            index = mid
+        else:
+            if val<lys[mid]:
+                last = mid -1
+            else:
+                first = mid +1
+    return index
+    BinarySearch([10,20,30,40,50]
+                 
+#Экспонециальный поиск
+def ExponentialSearch(lys, val):
+    if lys[0] == val:
+        return 0
+    index = 1
+    while index < len(lys) and lys[index] <= val:
+        index = index * 2
+    return BinarySearch( lys[:min(index, len(lys))], val)
+    print(ExponentialSearch([1,2,3,4,5,6,7,8],3))
+
